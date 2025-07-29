96a8373411797e7ab806ca77209a6b642bd19c8a
Rene Lopez <223045762+rene-jlopez@users.noreply.github.com>
2025-07-28 23:47:55 -0600

    funciones principales para los botones agregadas

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listadoAmigos = [];
+
+
+function agregarAmigo(){
+    let nombreAmigo = document.getElementById('amigo').value.trim();
+    console.log(nombreAmigo);
+
+    if(nombreAmigo == ''){
+        document.getElementById('btn-add').setAttribute('Disables', 'true');
+    }
+    else{
+
+        listadoAmigos.push(nombreAmigo);
+        console.log(listadoAmigos);
+        limpiarNombre();
+        imprimirLista(listadoAmigos);
+    }
+    return;
+}
+
+function limpiarNombre(){
+    document.getElementById('amigo').value='';
+}
+
+function imprimirLista(nombres){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = listadoAmigos;
+}
+
+function sortearAmigo(){
+    alert('wiiiiiii');
+}
+
+function enter(event){
+    if(event.key === 'Enter'){
+        agregarAmigo();
+    }
+}
